417b818e2831d4cd11c6de3e8ff20d4290e24548
Austin Wolfe <wolfe_austin@yahoo.com>
2018-04-22 18:45:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function printBadges(namesArray){
+  for(let num = 0; num < namesArray.length; num++){
+    console.log(`Welcome ${namesArray[num]}! You are employee #${num+1}.`);
+  }
+  return namesArray;
+}
+
+function tailsNeverFails(){
+  let numTimes = 0;
+  while(headsTails())
+  {
+    numTimes++;
+  }
+    return `You got ${numTimes} tails in a row!`
+
+}
+function headsTails () {
+  return Math.random() >= 0.5;
+}
